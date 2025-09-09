f8b74dda784e3c7992f1e7b4839d23e801c38787
Luis Riviere <larivier@syr.edu>
2025-09-09 19:16:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("HW Manager")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.set_page_config(page_title="Multi-Page Labs", page_icon="🧪")
+
+lab2 = st.Page("HW1.py", title="HW1")
+lab1 = st.Page("lab1.py", title="Lab 1")
+
+nav = st.navigation(pages=[lab2, lab1])   
+nav.run()
