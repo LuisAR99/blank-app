94a56de6a3fe8ac97de0fb581ee4b46240197a27
Luis Riviere <larivier@syr.edu>
2025-09-24 08:49:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Multi-Page Labs", page_icon="🧪")
 HW1 = st.Page("HW1.py", title="HW1")
 HW2 = st.Page("HW2.py", title="HW2")
 HW3 = st.Page("HW3.py", title="HW3")
+HW4 = st.Page("HW4.py", title="HW4")
 
-
-nav = st.navigation(pages=[HW3, HW2, HW1])   
+nav = st.navigation(pages=[HW4, HW3, HW2, HW1])   
 nav.run()
